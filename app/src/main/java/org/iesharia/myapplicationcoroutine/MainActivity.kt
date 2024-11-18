0cbcd0798c3bbefa88d93dd2727bfe6296c579b3
unai <unaiperez5665@gmail.com>
2024-11-18 09:28:01 +0000

    creada funcion CoroutineExampleScreen

--- app/src/main/java/org/iesharia/myapplicationcoroutine/MainActivity.kt
+++ app/src/main/java/org/iesharia/myapplicationcoroutine/MainActivity.kt
@@ -12,30 +12,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.iesharia.myapplicationcoroutine.ui.theme.MyApplicationCoroutinesTheme
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.delay
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApplicationCoroutinesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CoroutineExampleScreen()
+
                 }
             }
         }
-    }
-}
+
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CoroutineExampleScreen() {
+
 }
 
 @Preview(showBackground = true)
